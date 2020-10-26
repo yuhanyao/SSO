52d6da63f843dcc5cce4345c97e14d527481bbe1
Iteratorlittle <386938448@qq.com>
2020-10-26 20:42:49 +0800

    add filter file

---
+++ src/main/java/sso/server/filter/SSOServerFilter.java
@@ -0,0 +1,82 @@
+package sso.server.filter;
+
+import sso.server.JVMCache;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+public class SSOServerFilter implements Filter {
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        // 获取服务参数
+        String service = request.getParameter("service");
+        // 获取票据参数
+        String ticket = request.getParameter("ticket");
+        // 获取cookies
+        Cookie[] cookies = request.getCookies();
+        String username = "";
+        // cookie不为空
+        if (null != cookies) {
+            for (Cookie cookie : cookies) {
+                // 遍历所有的cookie名字 获取用户名
+                if ("sso".equals(cookie.getName())) {
+                    username = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (request.getRequestURI().contains("login")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 服务为空 并且票据不为空
+        if (Objects.nonNull(service)&& null != ticket) {
+            // 放行
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        // 用户名不为空
+        if (null != username && !"".equals(username)) {
+            long time = System.currentTimeMillis();
+            String timeString = username + time;
+            JVMCache.TICKET_AND_NAME.put(timeString, username);
+            StringBuilder url = new StringBuilder();
+            if (service == null) {
+                filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+                url.append(service);
+                if (0 <= service.indexOf("?")) {
+                    url.append("&");
+                } else {
+                    url.append("?");
+                }
+                url.append("ticket=").append(timeString);
+                // 获取用户名后重定向服务上的源地址
+                response.sendRedirect(url.toString());
+            }
+        } else {
+            if (service != null) {
+                request.setAttribute("service",service );
+            }
+            response.sendRedirect("/sso-server/login.jsp");
+        }
+    }
+}
